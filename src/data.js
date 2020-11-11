5a61546ff49e5b64745831404b1e125debe47a09
ChadHaley0384 <62257389+Chadwick0384@users.noreply.github.com>
2020-11-10 21:51:15 -0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,118 @@
+import { v4 as uuidv4 } from "uuid";
+function chillHop() {
+  return [
+    {
+      name: "Paved Paths",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/88e7eb711f8c71d87fc102e97cf91e36f692348d-1024x1024.jpg",
+      artist: "Leavv",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9919",
+      color: ["#448765", "#88745D"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "The Noise in Pictures",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/c209a7df7b9bc133dfff73ce86ebc3c57c2b73dd-1024x1024.jpg",
+      artist: "Sleepy Fish",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10035",
+      color: ["#1086A7", "#D5D3C2"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Paraglider",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/4fac6bb3a32f9e76b465990ba8d97d7db8a372f5-1024x1024.jpg",
+      artist: "Kendall Miles, H E R B",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9318",
+      color: ["#F7954E", "#1D160E"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Nightfall",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Reflection",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Under the City Stars",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+        name: "Fate",
+        cover:
+          "https://chillhop.com/wp-content/uploads/2020/06/faa452681fe33146c42a1b422fdf7cd7fd082637-1024x1024.jpg",
+        artist: "xander",
+        audio: "https://mp3.chillhop.com/serve.php/?mp3=8815",
+        color: ["#424961", "#A65645"],
+        id: uuidv4(),
+        active: false,
+      },
+      {
+        name: "Silience",
+        cover:
+          "https://chillhop.com/wp-content/uploads/2020/07/04dfa5fd3df93012cb2ae27065602fb944b43b91-1024x1024.jpg",
+        artist: "Toonorth",
+        audio: "https://mp3.chillhop.com/serve.php/?mp3=8425",
+        color: ["#4C3D6F", "#E2DDDF"],
+        id: uuidv4(),
+        active: false,
+      },
+      {
+        name: "Swing For Me",
+        cover:
+          "https://chillhop.com/wp-content/uploads/2020/04/35317644d6db24c3ca144619d03f2805fa702472-1024x1024.jpg",
+        artist: "SwuM",
+        audio: "https://mp3.chillhop.com/serve.php/?mp3=8010",
+        color: ["#6D8276", "#B86538"],
+        id: uuidv4(),
+        active: false,
+      },
+      {
+        name: "Forest Lake Walk",
+        cover:
+          "https://chillhop.com/wp-content/uploads/2020/07/9ba130514bcb5697e8a9be6a93fa2c8fc88223d0-1024x1024.jpg",
+        artist: "No Spirit",
+        audio: "https://mp3.chillhop.com/serve.php/?mp3=8508",
+        color: ["#B1A2B3", "#8B6E68"],
+        id: uuidv4(),
+        active: false,
+      },
+      {
+        name: "Morning In The Cafe",
+        cover:
+          "https://chillhop.com/wp-content/uploads/2020/07/9ba130514bcb5697e8a9be6a93fa2c8fc88223d0-1024x1024.jpg",
+        artist: "No Spirit",
+        audio: "https://mp3.chillhop.com/serve.php/?mp3=8068",
+        color: ["#B1A2B3", "#8B6E68"],
+        id: uuidv4(),
+        active: false,
+      },
+    //ADD MORE HERE
+  ];
+}
+
+export default chillHop;
